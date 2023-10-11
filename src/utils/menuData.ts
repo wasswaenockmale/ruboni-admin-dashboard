aceb83c754eb2a7a688b615648088949360e8d7e
wasswaenockmale <enockmale97@gmail.com>
2023-10-11 10:31:03 +0300

    menuData set up

---
+++ src/utils/menuData.ts
@@ -0,0 +1,49 @@
+// import { Overview } from "@/components/Icons";
+import {Analytics, Orders, Overview, Product, Rating, Rooms, Settings, Support, Users} from "@/components/Icons"
+export const menuData = [
+  {
+    name: "Overview",
+    href: "/dashboard",
+    icon: Overview
+  },
+  {
+    name: "Users",
+    href: "/dashboard/users",
+    icon: Users
+  },
+  {
+    name: "Products",
+    href: "/dashboard/products",
+    icon: Product
+  },
+  {
+    name: "Orders",
+    href: "/dashboard/orders",
+    icon: Orders
+  },
+  {
+    name: "R&A",
+    href: "/dashboard/rooms",
+    icon: Rooms
+  },
+  {
+    name: "Analytics",
+    href: "/dashboard/analytics",
+    icon: Analytics
+  },
+  {
+    name: "Rating",
+    href: "/dashboard/rating",
+    icon: Rating
+  },
+  {
+    name: "Support",
+    href: "/dashboard/support",
+    icon: Support,
+  },
+  {
+    name: "Settings",
+    href: "/dashboard/settings",
+    icon: Settings,
+  },
+];
