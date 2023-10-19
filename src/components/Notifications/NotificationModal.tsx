49fbc791efdae1c35e8e6d4a2c0d16d4b8e416fe
wasswaenockmale <enockmale97@gmail.com>
2023-10-19 16:06:22 +0300

    Notifications UI created

---
+++ src/components/Notifications/NotificationModal.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from 'react'
+
+function NotificationModal() {
+  return (
+    <div
+      className='absolute w-40 bg-white rounded-md shadow h-80'
+    >
+      <div
+        className='p-1 text-center border-b border-b-secondary-300'
+      >
+        <h2 className=' text-primary-500 font-roboto'>Notifications</h2>
+      </div>
+      
+    </div>
+  )
+}
+
+export default NotificationModal;
