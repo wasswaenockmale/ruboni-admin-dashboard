f50bb8f401dc1af6b817788a16b4a80d552d1c1a
wasswaenockmale <enockmale97@gmail.com>
2023-10-20 19:16:10 +0300

    feat: click outside the card that closes the card

--- src/components/NavBar/ProfileCard/ProfileCard.tsx
+++ src/components/NavBar/ProfileCard/ProfileCard.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useRef } from 'react'
 
 interface ProfileCardProps{
   showCard: boolean,
   handleCardDisplay: () => void
 }
-function ProfileCard({showCard, handleCardDisplay}:ProfileCardProps) {
+function ProfileCard({ showCard, handleCardDisplay }: ProfileCardProps) {
+
+  const profileCardRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => { 
+    function handleClickOutSide (event: MouseEvent) {
+      if (profileCardRef.current && !profileCardRef.current.contains(event.target as Node)) {
+        handleCardDisplay();
+      }
+    }
+    if (showCard) {
+      document.addEventListener('click', handleClickOutSide)
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutSide)
+    }
+  }, [showCard, handleCardDisplay]);
+
   return (
     <div
+      ref={profileCardRef as React.RefObject<HTMLDivElement>}
       className={`${showCard ? 'absolute bg-white border rounded-md shadow-lg right-3 top-8 h-72 border-secondary-50 z-10 w-60' : 'hidden'}`}
     >
       
