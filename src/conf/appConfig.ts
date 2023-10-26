127b5f4a3f5e21602f87d865a592e9d36a00987b
wasswaenockmale <enockmale97@gmail.com>
2023-10-26 20:51:10 +0300

    folder for project configurations

---
+++ src/conf/appConfig.ts
@@ -0,0 +1,8 @@
+const appConfig = {
+  appwriteUrl: String(process.env.
+    NEXT_PUBLIC_APPWRITE_API_URL),
+  appwriteProjectID: String(process.env.
+    NEXT_PUBLIC_APPWRITE_PROJECT_ID)
+}
+
+export default appConfig;
