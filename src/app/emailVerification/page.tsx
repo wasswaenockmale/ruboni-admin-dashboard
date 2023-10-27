804a2ab39fbaa253974b1980fde412dff414c44e
wasswaenockmale <enockmale97@gmail.com>
2023-10-27 21:42:17 +0300

    email verification

---
+++ src/app/emailVerification/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+function Page() {
+  return (
+    <div>
+      <p>You have successfully verified your email.</p>
+    </div>
+  )
+}
+
+export default Page
